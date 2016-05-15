b5b04f6336dbe6c698b37834d3fb2028a96cce22
Edward Schults <edward.schults@gmail.com>
2016-05-15 19:24:48 +0200

    added code in migration for production values

--- db/migrate/20160515164103_add_category_to_articles.rb
+++ db/migrate/20160515164103_add_category_to_articles.rb
@@ -1,5 +1,9 @@
 class AddCategoryToArticles < ActiveRecord::Migration
   def change
     add_column :articles, :category, :string
+    Article.all.each do |article|
+      article.category = "Chroniques judiciaires"
+      article.save
+    end
   end
 end
--- db/migrate/20160515165436_rename_hearing_held_on_in_articles.rb
+++ db/migrate/20160515165436_rename_hearing_held_on_in_articles.rb
@@ -1,5 +1,9 @@
 class RenameHearingHeldOnInArticles < ActiveRecord::Migration
   def change
     rename_column :articles, :hearing_held_on, :published_on
+    Article.all.each do |article|
+      article.published_on = article.created_at
+      article.save
+    end
   end
 end
